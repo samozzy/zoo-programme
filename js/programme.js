de80b3954414479261151392ce10326112127a29
samozzy <sam.tosborne@googlemail.com>
2022-05-15 17:12:21 +0000

    Deploying to gh-pages from @ samozzy/zoo-programme@dc6539e41464ee1a2fe15aa046883fd92aff817d 🚀

--- js/programme.js
+++ js/programme.js
@@ -1,12 +1,233 @@
 var filterBoxElement = document.getElementById('filter-box')
 filterBoxElement.addEventListener('show.bs.collapse', function () {
-  // toggle class 'active' of #filter-box-button 
-  document.getElementById('filter-box-button').classList.add('active');
-  console.log('added');
+	// toggle class 'active' of #filter-box-button 
+	document.getElementById('filter-box-button').classList.add('active');
+	console.log('added');
 })
 filterBoxElement.addEventListener('hide.bs.collapse', function () {
-  document.getElementById('filter-box-button').classList.remove('active');
-  console.log('removed');
+	document.getElementById('filter-box-button').classList.remove('active');
+	console.log('removed');
 })
 // TODO: This needs a narrower scope as it currently activates on *any* show.bs or hide.bs, as though
 // it's not quite listening to only events on filterBoxElement.
+
+// Set up for filter pickers 
+the_shows = document.getElementsByClassName('programme-show-container');
+var no_results_text = document.getElementById('no-result-text');
+
+// Search
+search_box = document.getElementById('search-input')
+search_box.addEventListener('input', (event) => {
+	// Sanitise text input 
+	search_term = search_box.value.toLowerCase().replace(' ', '');
+	search_hint = document.getElementById('search-hint');
+	// Do the search! 
+	if (search_term.length > 3){
+		search_hint.classList.add('d-none');
+		for (i=0; i<the_shows.length; i++){
+			if (!the_shows[i].dataset.search.includes(search_term)) {
+				the_shows[i].classList.add('filtered-out-by-search');
+			}
+			else {
+				the_shows[i].classList.remove('filtered-out-by-search');
+			}
+		}
+		if (document.querySelectorAll('.filtered-out-by-search').length == the_shows.length){
+			no_results_text.classList.remove('d-none');
+		}
+		else {
+			no_results_text.classList.add('d-none');
+		}
+
+	}
+	else {
+		no_results_text.classList.add('d-none');
+		for (i=0; i<the_shows.length; i++){
+			the_shows[i].classList.remove('filtered-out-by-search');
+		}
+		function showHint() {
+			if (search_term.length > 1 && search_term.length <= 3){ 
+				search_hint.classList.remove('d-none');
+			}
+		}
+		if (search_term.length > 1 && search_term.length <= 3){
+			// Wait a moment, then run the show script 
+			// The show script also runs an if to see if the value still matches
+			let hintTimeout = setTimeout(showHint, 2000)
+		}
+		else {
+			search_hint.classList.add('d-none');
+		}
+	}
+
+});
+
+
+// Price Filter 
+var price_checkboxes = document.querySelectorAll('input[name="filter-price-item"]');
+let picked_prices = [] 
+var filter_price_counter = document.getElementById('filter-price-counter');
+var price_buttons = document.getElementsByClassName('btn-price');
+
+function clearPriceCheckboxes() {
+	// Reset the checkboxes
+	for (const cbox in price_checkboxes) {
+		price_checkboxes[cbox].checked = false; 
+	}
+	// Show all the shows filtered by price (only)
+	for (i=0; i< the_shows.length; i++ ){
+		the_shows[i].classList.remove('filtered-out-by-price');
+	}
+	// Deactivate the buttons 
+	for (b=0; b< price_buttons.length; b++){
+		price_buttons[b].classList.remove('active');
+	}
+	// Hide the 'no results' text
+	no_results_text.classList.add('d-none');
+	// Hide the number of results text 
+	filter_price_counter.innerHTML = '';
+}
+
+price_checkboxes.forEach(function(checkbox) {
+  checkbox.addEventListener('change', function() {
+    picked_prices = 
+      Array.from(price_checkboxes) // Convert checkboxes to an array to use filter and map.
+      .filter(i => i.checked) // Use Array.filter to remove unchecked checkboxes.
+      .map(i => i.value) // Use Array.map to extract only the checkbox values from the array of objects.
+
+    // If all checkboxes are empty, show everything (rather than nothing)
+    if (picked_prices.length == 0) {
+			for (i=0; i< the_shows.length; i++ ) {
+				if (the_shows[i].classList.contains('filtered-out-by-price')) {
+					the_shows[i].classList.remove('filtered-out-by-price');
+				}
+			}
+			filter_price_counter.innerHTML = '';
+			no_results_text.classList.add('d-none')
+    }
+    else {
+    	// Assuming something is ticked, filter the shows appropriately. 
+
+    	// Hide all of them 
+    	for (const show of the_shows) {
+    		show.classList.add('filtered-out-by-price');
+    	}
+    	// Iterate over the picked prices and display shows that match
+    	for (p=0; p < picked_prices.length; p++) {
+    		var p_range = picked_prices[p].split(',');
+    		// STEP 1: Apply the filter to the show classes
+    		for (s=0; s < the_shows.length; s++) {
+    			if (p_range[0] <= parseFloat(the_shows[s].dataset.price) && parseFloat(the_shows[s].dataset.price) <= p_range[1]) {
+						// If the ticket price is in range, display the show 
+						if (the_shows[s].classList.contains('filtered-out-by-price')) the_shows[s].classList.remove('filtered-out-by-price');
+					}
+				}
+    	}
+	  	number_remaining = the_shows.length - document.getElementsByClassName('filtered-out-by-price').length
+	  	filter_price_counter.innerHTML = '(' + number_remaining + ')';
+    }
+
+	  // No results label
+    if (document.getElementsByClassName('filtered-out-by-price').length == the_shows.length) {
+    	// Everything filtered out 
+	    no_results_text.classList.remove('d-none');
+    }
+    else {
+    	no_results_text.classList.add('d-none');
+    }
+
+    // Make the button .active status follow the checkbox checked status explicitly 
+    var this_button = document.getElementById(this.id)
+    if (this_button.checked) {
+    	this.labels[0].classList.add('active');
+    }
+    else {
+    	this.labels[0].classList.remove('active');
+    }
+  })
+});
+
+// Genre Filter 
+var genre_checkboxes = document.querySelectorAll('input[name="filter-genre-item"]');
+let picked_genres = [] 
+var filter_genre_counter = document.getElementById('filter-genre-counter');
+var genre_buttons = document.getElementsByClassName('btn-genre');
+
+function clearGenreCheckboxes() {
+	// Reset the checkboxes
+	for (const cbox in genre_checkboxes) {
+		genre_checkboxes[cbox].checked = false; 
+	}
+	// Show all the shows filtered by genre (only)
+	for (i=0; i< the_shows.length; i++ ){
+		the_shows[i].classList.remove('filtered-out-by-genre');
+	}
+	// Deactivate the buttons 
+	for (b=0; b< genre_buttons.length; b++){
+		genre_buttons[b].classList.remove('active');
+	}
+	// Hide the 'no results' text
+	no_results_text.classList.add('d-none');
+	// Hide the number of results text 
+	filter_genre_counter.innerHTML = '';
+}
+
+genre_checkboxes.forEach(function(checkbox) {
+  checkbox.addEventListener('change', function() {
+    picked_genres = 
+      Array.from(genre_checkboxes) // Convert checkboxes to an array to use filter and map.
+      .filter(i => i.checked) // Use Array.filter to remove unchecked checkboxes.
+      .map(i => i.value) // Use Array.map to extract only the checkbox values from the array of objects.
+
+    // If all checkboxes are empty, show everything (rather than nothing)
+    if (picked_genres.length == 0) {
+			for (i=0; i< the_shows.length; i++ ) {
+				if (the_shows[i].classList.contains('filtered-out-by-genre')) {
+					the_shows[i].classList.remove('filtered-out-by-genre');
+				}
+			}
+			filter_genre_counter.innerHTML = '';
+			no_results_text.classList.add('d-none')
+    }
+    else {
+    	// Assuming something is ticked, filter the shows appropriately. 
+
+    	// Hide all of them 
+    	for (const show of the_shows) {
+    		show.classList.add('filtered-out-by-genre');
+    	}
+    	// Iterate over the picked genres and display shows that match
+    	for (p=0; p < picked_genres.length; p++) {
+    		// STEP 1: Apply the filter to the show classes
+    		console.log(picked_genres);
+    		for (s=0; s < the_shows.length; s++) {
+    			if (the_shows[s].dataset.genre.includes(picked_genres[p])) {
+						// If the ticket price is in range, display the show 
+						if (the_shows[s].classList.contains('filtered-out-by-genre')) the_shows[s].classList.remove('filtered-out-by-genre');
+					}
+				}
+    	}
+	  	number_remaining = the_shows.length - document.getElementsByClassName('filtered-out-by-genre').length
+	  	filter_genre_counter.innerHTML = '(' + number_remaining + ')';
+    }
+
+	  // No results label
+    if (document.getElementsByClassName('filtered-out-by-genre').length == the_shows.length) {
+    	// Everything filtered out 
+	    no_results_text.classList.remove('d-none');
+    }
+    else {
+    	no_results_text.classList.add('d-none');
+    }
+
+    // Make the button .active status follow the checkbox checked status explicitly 
+    var this_button = document.getElementById(this.id)
+    if (this_button.checked) {
+    	this.labels[0].classList.add('active');
+    }
+    else {
+    	this.labels[0].classList.remove('active');
+    }
+  })
+});
+
